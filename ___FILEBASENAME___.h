8ebefc0b668d367785601864979de7732ecd6bc4
Dylan Shine <dylanshinenyc@gmail.com>
2015-06-26 15:43:42 -0400

    Core Data stack custom file template

---
+++ ___FILEBASENAME___.h
@@ -0,0 +1,23 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@interface CoreDataStack : NSObject
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
++(instancetype) sharedStack;
+
+@end
